86925665170b5ec989bef31c7dc22e02f45fe2c5
Tanique Raynor <tanique.raynor@gmail.com>
2022-02-06 13:25:31 -0500

    created my first Flask app

--- app.py
+++ app.py
@@ -24,6 +24,10 @@ def page_not_found(error):
 def home():
     return 'My Home Page'
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 app.run(debug=True, host="0.0.0.0", port=8080)
 
